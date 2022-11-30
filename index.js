e9f0fb5b1be6d57ed594ad1b1eed80dee69cf187
sainad82 <20pa1a0582@vishnu.edu.in>
2022-11-30 22:17:49 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
-function hello(){
-    console.log("Hello Node.js!");
-}
+/* eslint-disable n/handle-callback-err */
+/* eslint-disable no-unused-vars */
+const http = require('http')
+const fs = require('fs')
 
-hello();
+let homeContent = ''
+let projectContent = ''
+let regestrationContent = ''
 
-let hell = "Hello!";
+const args = require('minimist')(process.argv.slice(1))
 
-returnHell = () =>{
-    console.log(hell);
-}
+fs.readFile('home.html', (err, data) => {
+  if (err) {
+    throw err
+  }
+  homeContent = data
+})
 
-returnHell();
+fs.readFile('project.html', (err, project) => {
+  if (err) {
+    throw err
+  }
+  projectContent = project
+})
+
+fs.readFile('registration.html', (err, reg) => {
+  if (err) {
+    throw err
+  }
+  regestrationContent = reg
+})
+http.createServer((request, response) => {
+  const url = request.url
+  response.writeHeader(200, { 'Content-Type': 'text/html' })
+  switch (url) {
+    case '/registration':
+      response.write(regestrationContent)
+      response.end()
+      break
+    case '/project':
+      response.write(projectContent)
+      response.end()
+      break
+    default:
+      response.write(homeContent)
+      response.end()
+      break
+  }
+}).listen(args.port)
